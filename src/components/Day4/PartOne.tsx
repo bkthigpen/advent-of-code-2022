73a4dbbe332902d2f33173408923b7de15f8f97b
Brandon Thigpen <brandonkthigpen@gmail.com>
2022-12-04 19:54:35 -0500

    Commits Day 4 Part 1 Solution

--- src/components/Day4/PartOne.tsx
+++ src/components/Day4/PartOne.tsx
@@ -3,33 +3,17 @@ import puzzleInput from '@data/day4/puzzleInput';
 const splitPuzzle = puzzleInput.split('\n').map((input) => input.split(','));
 
 const determineWorkOverlap = splitPuzzle.map((puzzle) => {
-  const firstElf = puzzle[0].split('-');
-  const secondElf = puzzle[1].split('-');
-
-  const orderedElvesArray =
-    firstElf[0] > secondElf[0] ? [secondElf, firstElf] : [firstElf, secondElf];
-
-  const firstElfSorted = orderedElvesArray[0];
-  const secondElfSorted = orderedElvesArray[1];
+  const firstElf = puzzle[0].split('-').map((elf) => parseInt(elf));
+  const secondElf = puzzle[1].split('-').map((elf) => parseInt(elf));
 
   const hasWorkOverlap: number =
-    firstElfSorted[0] <= secondElfSorted[0] &&
-    firstElfSorted[1] >= secondElfSorted[1]
+    (firstElf[0] <= secondElf[0] && firstElf[1] >= secondElf[1]) ||
+    (firstElf[0] >= secondElf[0] && firstElf[1] <= secondElf[1])
       ? 1
       : 0;
 
-  // console.log('firstElf', firstElf);
-  // console.log('secondElf', secondElf);
-  // console.log('orderElvesArray', orderedElvesArray);
-
-  // console.log('firstElfSorted', firstElfSorted);
-  // console.log('secondElfSorted', secondElfSorted);
-  // console.log('hasWorkOverlap', hasWorkOverlap);
-  // console.log('-------');
   return hasWorkOverlap;
 });
-
-// console.log('determineWorkOverlap', determineWorkOverlap);
 
 const PartOne = () => {
   const totalWorkOverlap = determineWorkOverlap.reduce(
@@ -37,12 +21,12 @@ const PartOne = () => {
     0
   );
 
-  {
-    /* 511 - This is incorrect so find where the edge case is */
-  }
-
-  console.log('totalWorkOverlap', totalWorkOverlap);
-  return <div>{totalWorkOverlap}</div>;
+  return (
+    <div>
+      {/* Answer - 538 */}
+      {totalWorkOverlap}
+    </div>
+  );
 };
 
 export default PartOne;
